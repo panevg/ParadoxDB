283a7eeba62d4f2b913bba34cdf768a6b5c75d43
Pan. Evg. <PinBibe@proton.me>
2026-07-19 16:37:12 +0200

    basic cli handler done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello via Bun!");
+import pkg from "../package.json";
+
+import { Signale } from "signale";
+import cac from "cac";
+import envPaths from "env-paths";
+
+const log: Signale = new Signale();
+
+const cli = cac();
+
+const paths = envPaths(pkg.build.filename, {
+  suffix: "",
+});
+
+cli.name = pkg.build.filename;
+cli.version(pkg.version);
+
+const mode: "dev" | "prod" =
+  process.execPath.endsWith("bun") || process.execPath.endsWith("bun.exe")
+    ? "dev"
+    : "prod";
+
+cli
+  .command("new-db", "Create a new database.")
+  .option("-n, --name <name>", "Database name.")
+  .option("-v, --vault <path>", "Database location.")
+  // .option("--", ".")
+  .action((options) => {
+    console.log(options);
+  });
+
+cli.help();
+cli.parse();
